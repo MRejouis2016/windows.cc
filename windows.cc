1ac94705536078736d5c1f4808d9498c05e104d9
Mcvoltrudge Rejouis <37338175+MRejouis2016@users.noreply.github.com>
2018-04-04 23:00:01 -0400

    Add files via upload

--- windows.cc
+++ windows.cc
@@ -1,1 +1,39 @@
-# windows.cc
+void Windows::setHomescreen(string)
+{
+  string homescreen;
+}
+
+void Windows::setMenu(string)
+{
+  string menu;
+}
+
+void Windows::setProgram(string)
+{
+  string program;
+}
+
+void Windows::setStart(char)
+{
+  string start;
+}
+
+}string Windows::getHomescreen() const
+{
+  return homescreen;
+}
+
+string Windows::getMenu() const
+{
+  return menu;
+}
+
+string BaseballPlayer::getProgram() const
+{
+  return program;
+}
+
+double BaseballPlayer::getStart() const
+{
+  return start;
+}
